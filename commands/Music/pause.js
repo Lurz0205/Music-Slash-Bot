0fed752d4a7bf33b883b0f91db6d205a47d8a716
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-05-20 09:43:57 +0700

    Update pause.js

--- commands/Music/pause.js
+++ commands/Music/pause.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "pause",
-    description: "Pauses the currently playing track",
+    description: "Tạm dừng bản nhạc hiện tại",
     timeout: 5000,
     run: async (interaction, client) => {
         const queue = await client.distube.getQueue(interaction)
         const voiceChannel = interaction.member.voice.channel
         if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+            return interaction.reply({ content: "Cậu phải tham gia vào một kênh thoại trước đã!", ephemeral: true })
         }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
@@ -17,15 +17,15 @@ module.exports = {
             return interaction.reply({ embeds: [queueError] })
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
+            return interaction.reply({ content: "Cậu đang không ở cùng kênh thoại với tớ mà!", ephemeral: true })
         }
         try {
             await client.distube.pause(interaction)
-            await interaction.reply("***Paused the current track***")
+            await interaction.reply("***Tớ đã dừng bản nhạc lại rùi đó***")
             const message = await interaction.fetchReply()
             await message.react("⏸")
         } catch {
-            interaction.reply({ content: " The queue has been paused already", ephemeral: true })
+            interaction.reply({ content: " Bản nhạc đã được dừng lại rùi đó", ephemeral: true })
         }
     }
 }
