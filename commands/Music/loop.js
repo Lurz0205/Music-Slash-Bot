6a75ed65afc98db15f77e2113b4b206b8e9fa852
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-05-20 09:30:56 +0700

    Update loop.js

--- commands/Music/loop.js
+++ commands/Music/loop.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "loop",
-    description: "Loops throught current song",
+    description: "Bật chế độ lặp cho bản nhạc hiện tại",
     timeout: 5000,
     run: async (interaction, client) => {
         const queue = await client.distube.getQueue(interaction)
         const voiceChannel = interaction.member.voice.channel
         if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+            return interaction.reply({ content: "Cậu hãy tham gia vào một kênh thoại đã chứ!", ephemeral: true })
         }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
@@ -17,7 +17,7 @@ module.exports = {
             return interaction.reply({ embeds: [queueError] })
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
+            return interaction.reply({ content: "Cậu đang không ở cùng kênh thoại với tớ mà!", ephemeral: true })
         }
         let mode = client.distube.setRepeatMode(interaction)
         mode = mode ? mode === 2 ? "Repeat queue" : "Repeat song" : "Off"
