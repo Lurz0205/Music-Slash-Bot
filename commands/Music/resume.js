b5f50dd34eab89c09b85e7798eb719053e5d5219
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-05-20 09:35:18 +0700

    Update resume.js

--- commands/Music/resume.js
+++ commands/Music/resume.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "resume",
-    description: "Resumes the currently playing track",
+    description: "Tiếp tục phát bản nhạc hiện tại",
     timeout: 5000,
     run: async (interaction, client) => {
         const queue = await client.distube.getQueue(interaction)
         const voiceChannel = interaction.member.voice.channel
         if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+            return interaction.reply({ content: "Hãy tham gia vào một kênh thoại trước nha!", ephemeral: true })
         }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
@@ -17,7 +17,7 @@ module.exports = {
             return interaction.reply({ embeds: [queueError] })
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
+            return interaction.reply({ content: "Cậu đang không ở cùng kênh thoại với tớ mà!", ephemeral: true })
         }
         try {
             await client.distube.resume(interaction)
@@ -25,7 +25,7 @@ module.exports = {
             const message = await interaction.fetchReply()
             await message.react("▶")
         } catch {
-            interaction.reply({ content: "The queue has been playing already", ephemeral: true })
+            interaction.reply({ content: "Bản nhạc hiện tại đang được phát rùi", ephemeral: true })
         }
     }
 }
