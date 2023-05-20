7d6c9b15513d5d21114109b136f894067f728fcc
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-05-20 09:41:39 +0700

    Update autoplay.js

--- commands/Music/autoplay.js
+++ commands/Music/autoplay.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js")
 
 module.exports = {
     name: "autoplay",
-    description: "Enable or disable autoplay",
+    description: "Bật hoặc tắt tự động phát",
     timeout: 5000,
     run: async (interaction, client) => {
         const queue = await client.distube.getQueue(interaction)
         const voiceChannel = interaction.member.voice.channel
         if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+            return interaction.reply({ content: "Cậu phải tham gia vào một kênh thoại trước đã!", ephemeral: true })
         }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
@@ -17,9 +17,9 @@ module.exports = {
             return interaction.reply({ embeds: [queueError] })
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
+            return interaction.reply({ content: "Cậu đang không ở cùng kênh thoại với tớ mà!", ephemeral: true })
         }
         const mode = client.distube.toggleAutoplay(interaction)
-        return interaction.reply("Set autoplay mode to `" + (mode ? "On" : "Off") + "`")
+        return interaction.reply("Đã đặt chế độ tự đông phát thành `" + (mode ? "On" : "Off") + "`")
     }
 }
