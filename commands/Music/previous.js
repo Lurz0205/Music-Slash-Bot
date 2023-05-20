605e6e061a49ed2e431b427b0ffefb4407fd5e01
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2023-05-20 09:45:42 +0700

    Update previous.js

--- commands/Music/previous.js
+++ commands/Music/previous.js
@@ -8,7 +8,7 @@ module.exports = {
         const queue = await client.distube.getQueue(interaction)
         const voiceChannel = interaction.member.voice.channel
         if (!voiceChannel) {
-            return interaction.reply({ content: "Please join a voice channel!", ephemeral: true })
+            return interaction.reply({ content: "Cậu phải tham gia một kênh thoại trước đã!", ephemeral: true })
         }
         if (!queue) {
             const queueError = new Discord.MessageEmbed()
@@ -17,7 +17,7 @@ module.exports = {
             return interaction.reply({ embeds: [queueError] })
         }
         if (interaction.member.guild.me.voice.channelId !== interaction.member.voice.channelId) {
-            return interaction.reply({ content: "You are not on the same voice channel as me!", ephemeral: true })
+            return interaction.reply({ content: "Cậu đang không ở cùng kênh thoại với tớ mà!", ephemeral: true })
         }
         try {
             await client.distube.previous(interaction)
